e5a8c6c588e7b105f824d9c60fc5dcfd1a5d1897
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2021-11-19 22:37:08 +0100

    Cart Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+// import classes from './Meals.module.css';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+import { Fragment } from 'react';
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvailableMeals />
+        </Fragment>
+    )
+}
+
+export default Meals;
